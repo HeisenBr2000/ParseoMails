b1630bf5484032248f271ba81c0f95dd3fd3ff7e
HeisenBr2000 <seb.larag@duocuc.cl>
2024-05-13 10:40:38 -0400

    Cambios

--- parse.py
+++ parse.py
@@ -3,6 +3,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 import os
 import base64
+import re
 
 api = FastAPI()
 
@@ -21,9 +22,27 @@ def credenciales():
     )
     return credencial
 
-credencial = credenciales()
 
+
+def obtener_encabezado(headers, name):
+    for header in headers:
+        if header['name'] == name:
+            return header['value']
+    return None
+
+# Quitar caracteres de escape con regex 
+def obtener_cuerpo(message):
+    if 'parts' in message['payload']:
+        parts = message['payload']['parts']
+        for part in parts:
+            if part['mimeType'] == 'text/plain':
+                data = part['body']['data']
+                body = base64.urlsafe_b64decode(data).decode('utf-8')
+                body = re.sub(r'[\r\n]', '', body)
+                return body
+            
 def obtener_ultimo_correo():
+    credencial = credenciales()
     service = build('gmail', 'v1', credentials=credencial)
     # Muestra la lista de mensajes/correos que han llegado
     results = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
@@ -33,13 +52,11 @@ def obtener_ultimo_correo():
         # Recupera el ultimo mensaje/correo que ha llegado
         message = service.users().messages().get(userId='me', id=message_id).execute()
         headers = message['payload']['headers']
-        subject = next((header['value'] for header in headers if header['name'] == 'Subject'), None)
-        sender = next((header['value'] for header in headers if header['name'] == 'From'), None)
-        body = message['snippet']
-        return {"Asunto": subject, "Remitente": sender, "Cuerpo": body}
-    else:
-        return {"message": "No se encontraron correos en la bandeja de entrada"}
+        subject = obtener_encabezado(headers, 'Subject')
+        sender = obtener_encabezado(headers, 'From')
+        body = obtener_cuerpo(message)
 
+        return {"Asunto": subject, "Remitente": sender, "Cuerpo": body}
 
 @api.post("/")
 async def notificacionesUser(request: Request):
@@ -47,6 +64,10 @@ async def notificacionesUser(request: Request):
     detalles_correo = obtener_ultimo_correo()
     print("Tienes un correo entrante...", detalles_correo)
     return {}
+
+# Interfaz de usuario en la cual el usuario ingrese sus credenciales de correo electronico y contraseña, luego la app utilizara
+# esas credenciales para conectarse al servidor del correo y recibir notificaciones sobre nuevos correos electronicos
+
 
 
 
